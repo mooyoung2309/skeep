c7dbb717cb88de636fe3264311862e7dab03100a
송영모 <mooyoung2309@gmail.com>
2023-03-26 12:44:09 +0900

    refactor: 불필요한 파일 제거

---
+++ Projects/FeatureHabit/Targets/FeatureHabit/HabitView.swift
@@ -0,0 +1,23 @@
+//
+//  HabitView.swift
+//  FeatureHabit
+//
+//  Created by 송영모 on 2023/03/26.
+//  Copyright © 2023 Copynote. All rights reserved.
+//
+
+import SwiftUI
+
+public struct HabitView: View {
+    public init() {}
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HabitView_Previews: PreviewProvider {
+    static var previews: some View {
+        HabitView()
+    }
+}
